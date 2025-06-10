8060a5d0dbfa747fc2dd7f6e84b8c79623c721b1
DakotaH5000 <DakotaH5000@gmail.com>
2025-06-09 21:45:20 -0400

    Created csvs and roadmap

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+#1 is hit or take action, 0 is false or no action, 2 is double down
+
+#import files
+#stores with headers and insert card total in [x][0] of every row
+
+#compare cards, determine split, determine soft or hard total, then determine action, take actinos as neceesary
+
+#generate hands
+
+#compare hands and run simulation
